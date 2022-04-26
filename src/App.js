95efd55c177d127cf1491e8645c1170bd6f168dc
AvesSol <avessolanki043@gmail.com>
2022-04-26 13:29:19 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ColorPicker from "./Color.js";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function App() {
+  const [copy, setCopy] = useState("");
+
+  const copyOn = (copynow) => {
+    setCopy(copynow);
+    console.log(copynow)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="top">
+        <h3 className="tlhead">UIColorPicker</h3>
+        <h3 className="tldes">About LCO</h3>
+      </div>
+      <div className="middle">
+        <h3 className="mhead">Click to Copy</h3>
+        <h3 className="mdes">Always Perfect Color</h3>
+      </div>
+      <div className="bottom">
+        <div className="btop">
+          <h3 className="bleft">Feeling Red</h3>
+          <button className="btn">Gradient</button>
+        </div>
+
+        <div className="cardsdiv">
+          {ColorPicker.map((item) => (
+            <>
+              <div className="bleft item" style={{ display: `${item.dis}` }}>
+                <span>{item.heading}</span>
+              </div>
+
+              {item.coloritem.map((sub) => (
+                <>
+                  <div className="cards">
+                    <CopyToClipboard text={`${sub.color}`}>
+                      <div
+                        className="carditem"
+                        style={{ backgroundColor: `${sub.color}` }}
+                        onClick={() => copyOn(sub.color)}
+                      ></div>
+                    </CopyToClipboard>
+                    {copy === `${sub.color}` ? (
+                      <div className="carddes card1">copied <input className="checked" type="checkbox"  checked="true"  /></div>
+                    ) : (
+                      <div className="carddes">{sub.color} </div>
+                    )}
+                  </div>
+                </>
+              ))}
+            </>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Color.js
@@ -0,0 +1,88 @@
+const colorPicker = [
+  {
+    dis: "none" ,
+    coloritem: [
+      { color: "#FF6263" },
+      { color: "#bf3325" },
+      { color: "#e21717" },
+      { color: "#bf3312" },
+      { color: "#d82f2f" },
+      { color: "#ed4d4b" },
+      { color: "#ef5354" },
+      { color: "#b4161b" },
+      { color: "#e7425e" },
+      { color: "#e83a59" },
+      { color: "#d9345a" },
+      { color: "#de4839" }
+    ],
+  },
+  {
+    heading: "Filling Blue",
+    coloritem: [
+      { color: "#1b98f5" },
+      { color: "#23c4ed" },
+      { color: "#383cc1" },
+      { color: "#12b0e8" },
+      { color: "#51e1ed" },
+      { color: "#207398" },
+      { color: "#46b2e0" },
+      { color: "#3944f7" },
+      { color: "#2827cc" },
+      { color: "#5da3fa" },
+      { color: "#03203C" },
+      { color: "#120e43" }
+    ],
+  },
+  {
+    heading: "Filling Green",
+    coloritem: [
+      { color: "#1FAA59" },
+      { color: "#66AD47" },
+      { color: "#02B290" },
+      { color: "#50DBB4" },
+      { color: "#3DBE29" },
+      { color: "#4DD637" },
+      { color: "#1C8D73" },
+      { color: "#66AD47" },
+      { color: "#6AC47E" },
+      { color: "#22CB5C" },
+      { color: "#00D84A" },
+      { color: "#6EC72D" }
+    ],
+  },
+  {
+    heading: "Filling Yellow",
+    coloritem: [
+      { color: "#E8BD0D" },
+      { color: "#EDBF69" },
+      { color: "#F4BE2C" },
+      { color: "#EDC126" },
+      { color: "#E5D68A" },
+      { color: "#DDD101" },
+      { color: "#D9D55B" },
+      { color: "#F7CD2E" },
+      { color: "#AF9D5A" },
+      { color: "#F4CE6A" },
+      { color: "#A77B06" },
+      { color: "#C7C11A" }
+    ],
+  },
+  {
+    heading: "Filling Fancy",
+    coloritem: [
+      { color: "#8D3DAF" },
+      { color: "#E07C24" },
+      { color: "#758283" },
+      { color: "#E03B8B" },
+      { color: "#FF6666" },
+      { color: "#242B2E" },
+      { color: "#03C6C7" },
+      { color: "#5A20CB" },
+      { color: "#6A1B4D" },
+      { color: "#CAD5E2" },
+      { color: "#35BDD0" },
+      { color: "#0D0D0D" }
+    ],
+  }
+];
+export default colorPicker;
